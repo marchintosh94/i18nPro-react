dc9061de9fbb830518c5f7f44a1c410886b54cbf
Marco Baratto <marcobaratto@icloud.com>
2024-10-04 01:49:39 +0200

    Related #5: Solved problem related to update UI state once the locale property updates
    - doc: improvued documentation
    - refactoring: simplified existing code
    
    fix #5

--- src/test/i18nProcontext.spec.tsx
+++ src/test/i18nProcontext.spec.tsx
@@ -2,32 +2,29 @@ import React, { useEffect } from 'react'
 import { act, fireEvent, render, screen, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { I18nProProvider, useI18nProContext } from '../context/i18nProContext'
-import { i18nPro } from '@marchintosh94/i18n-pro'
 
 describe('I18nProContext', () => {
-  i18nPro.defaultLocale = 'fr'
   afterEach(() => {
     jest.clearAllMocks()
   })
 
   it('should render children and provide context values', () => {
-    render(
-      <I18nProProvider>
-        <div>Test Child</div>
-      </I18nProProvider>
-    )
-
+    act(() => {
+      render(
+        <I18nProProvider
+          initialSetup={{ locale: 'en', messages: { key: 'value' } }}
+        >
+          <div>Test Child</div>
+        </I18nProProvider>
+      )
+    })
     expect(screen.getByText('Test Child')).toBeInTheDocument()
   })
 
   it('should provide correct context values and update locale on load', () => {
     const TestComponent = () => {
-      const { locale, switchLoadLanguage, t } = useI18nProContext()
-      useEffect(() => {
-        switchLoadLanguage('en-us', {
-          change_language: 'change language'
-        })
-      }, [])
+      const { switchLoadLanguage, locale, t } = useI18nProContext()
+
       return (
         <div>
           <span>{locale}</span>
@@ -43,19 +40,44 @@ describe('I18nProContext', () => {
         </div>
       )
     }
-
     const { container } = render(
-      <I18nProProvider>
+      <I18nProProvider initialSetup={{ locale: 'fr', messages: {} }}>
         <TestComponent />
       </I18nProProvider>
     )
-
-    expect(screen.getByText('fr')).toBeInTheDocument()
-    expect(screen.getByRole('button')).toHaveTextContent('change_language')
     waitFor(() => {
-      expect(screen.getByText('en-us')).toBeInTheDocument()
-      expect(screen.getByRole('button')).toHaveTextContent('change language')
-      console.log(container.innerHTML)
+      expect(screen.getByText('fr')).toBeInTheDocument()
+      expect(screen.getByRole('button')).toHaveTextContent('change_language')
+    })
+  })
+
+  it('should provide correct context values, update locale on load and load tranlsation from path', () => {
+    const TestComponent = () => {
+      const { switchLoadLanguage, locale, t } = useI18nProContext()
+
+      return (
+        <div>
+          <span>{locale}</span>
+          <button
+            onClick={() =>
+              switchLoadLanguage('es', {
+                change_language: 'cambiar el idioma'
+              })
+            }
+          >
+            {t('change_language')}
+          </button>
+        </div>
+      )
+    }
+    const { container } = render(
+      <I18nProProvider initialSetup={{ locale: 'fr', path: '' }}>
+        <TestComponent />
+      </I18nProProvider>
+    )
+    waitFor(() => {
+      expect(screen.getByText('fr')).toBeInTheDocument()
+      expect(screen.getByRole('button')).toHaveTextContent('change_language')
     })
   })
   it('should provide update locale when new laguage has been loaded', () => {
@@ -77,13 +99,13 @@ describe('I18nProContext', () => {
         </div>
       )
     }
-
     const { container } = render(
-      <I18nProProvider>
+      <I18nProProvider
+        initialSetup={{ locale: 'fr', messages: { test: 'test' } }}
+      >
         <TestComponent />
       </I18nProProvider>
     )
-
     act(() => {
       fireEvent.click(screen.getByRole('button'))
     })
@@ -91,24 +113,5 @@ describe('I18nProContext', () => {
       expect(screen.getByText('es')).toBeInTheDocument()
       expect(screen.getByRole('button')).toHaveTextContent('cambiar el idioma')
     })
-  })
-
-  it('should throw error when useI18nProContext is used outside of I18nProProvider', () => {
-    const TestComponent = () => {
-      try {
-        useI18nProContext()
-      } catch (e) {
-        return <div>{(e as Error).message}</div>
-      }
-      return <></>
-    }
-
-    render(<TestComponent />)
-
-    expect(
-      screen.getByText(
-        'useI18nProContext must be used within an I18nProProvider'
-      )
-    ).toBeInTheDocument()
   })
 })
--- src/types/context.ts
+++ src/types/context.ts
@@ -1,3 +1,12 @@
 import { useI18nPro } from '../hook/useI18nPro'
 
 export type I18nProContextType = ReturnType<typeof useI18nPro>
+
+export type InitialSetup = { locale: string } & (
+  | { path: string; messages?: never }
+  | { messages: Record<string, string | number>; path?: never }
+)
+
+export interface I18nProProviderProps {
+  initialSetup: InitialSetup
+}
